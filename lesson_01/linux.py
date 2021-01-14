1e0ae310e249b8d23eac109f1484d23fc14293da
Artem Zorin <marketing@semexperts.com.ua>
2021-01-15 01:24:49 +0200

    New homework 8

---
+++ lesson_01/linux.py
@@ -0,0 +1,29 @@
+pwd - way to direct
+ls - content dir
+clear - clear bash
+ls -hal - show invis objects
+cat <file> - show file
+// double tab - show command
+touch <file> - ceate file
+cd - move to dir
+env - переменные окружения
+cd ~ - home dir
+brew install <name> - instal app
+mc - start midnight commander
+whoami - user name
+uptime - how long cac works?
+uname - system info
+hostname -
+which <command name> -  dir of comand
+mkdir <name> - create dir
+rm <name> - delete file
+rm -rf <namedir> - удаляет папку и все содрежимое
+df -h - показывает свободное место на диске
+top - show process
+htop - show process in human view
+ctrl+c - exit
+grep <file>- find in file
+| - разделяет команды, чтоб выполнялись последователь
+> <file> - создает и добавляет результат в файл
+>> <file> - просто добавляет результат в файл
+nano - text editor
